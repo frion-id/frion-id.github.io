58d32a63d01be979ff2260c4b1b49d2441ee3a05
frion-id <idfrion@gmail.com>
2025-09-28 23:35:25 +0700

    Update script.js

--- chatbot/script.js
+++ chatbot/script.js
@@ -26,7 +26,7 @@ async function sendMessage() {
 
     try {
         // Mengirim pertanyaan ke "kurir aman" kita
-        const response = await fetch('/.netlify/functions/diagnose-ac', {
+        const response = await fetch('/api/diagnose-ac', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ message: userMessage })
